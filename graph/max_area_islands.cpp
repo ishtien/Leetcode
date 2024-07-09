3e7846905ae9f4152600e13e0ffb1ee938b4b7bd
ishtien <203isthebest@gmail.com>
2024-07-09 17:52:12 +0800

    Add solutions for count max area of the islands.

---
+++ graph/max_area_islands.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int max_area = 0;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 1) {
+                    int area = 0;
+                    dfs(i, j, m, n, grid, area);
+                    max_area = max(max_area, area);
+                }
+            }
+        }
+        return max_area;
+    }
+    
+    void dfs(int row, int col, int m, int n, vector<vector<int>>& grid, int& count) {
+        if(row < 0 || col < 0 || row >= m || col >= n || grid[row][col] == 0) return;
+        count++;
+        grid[row][col] = 0;
+        dfs(row + 1, col, m, n, grid, count);
+        dfs(row - 1, col, m, n, grid, count);
+        dfs(row, col + 1, m, n, grid, count);
+        dfs(row, col - 1, m, n, grid, count);
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<vector<int>> grid = {
+        {0, 0, 1, 0, 0},
+        {0, 1, 1, 1, 0},
+        {0, 1, 0, 1, 0},
+        {0, 1, 1, 1, 0},
+        {0, 0, 0, 0, 0}
+    };
+    
+    int max_area = sol.maxAreaOfIsland(grid);
+    cout << "Maximum area of island: " << max_area << endl;
+
+    return 0;
+}
